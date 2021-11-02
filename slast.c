e68a86e761d7b1f171aea7a83c6958fef64fbdac
Tal <talzhtc@gmail.com>
2021-11-02 21:34:19 +0200

    Done

--- slast.c
+++ slast.c
@@ -3,104 +3,23 @@
 #include<utmp.h>
 #include<malloc.h>
 #include<time.h>
-<<<<<<< HEAD
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-
-typedef struct {
-  string *array;
-  size_t used;
-  size_t size;
-} Array;
-
-void initArray(Array *a, size_t initialSize) {
-  a->array = malloc(initialSize * sizeof(int));
-  a->used = 0;
-  a->size = initialSize;
-}
-
-void insertArray(Array *a, int element) {
-  // a->used is the number of used entries, because a->array[a->used++] updates a->used only after the array has been accessed.
-  // Therefore a->used can go up to a->size 
-  if (a->used == a->size) {
-    a->size *= 2;
-    a->array = realloc(a->array, a->size * sizeof(int));
-  }
-  a->array[a->used++] = element;
-}
-=======
 #include<string.h>
 #include<stdlib.h>
 #include<unistd.h>
 
->>>>>>> 60d54ebaf43d259386bc4c34169a95dd1ccb0239
+#define VALID_ARGC 2
+#define FIRST_ARGV 1
 
 int main(int argc, char *argv[])
 { 
-    if( argc != 2 ){
+    if( argc != VALID_ARGC){
         printf("invalid argument: must run in this format:\n");
         printf("\"./slast n\"\n");
         printf("where n is integer\n");
         exit(1);
     }
 
-<<<<<<< HEAD
-int main(int argc, char *argv[])
-{
-
-  if (argc != 2 ){
-    printf("This program takes a single parameter\n");
-    exit(1);
-  }
-  int num_of_entries = atoi(argv[1]);
-  int fd;
-  struct utmp cr;
-  size_t reclen = sizeof(struct utmp);
-
-  fd = open(WTMP_FILE, O_RDONLY);
-  if (fd == -1){
-      perror("Couldn't open file");
-      exit(1);
-  }
-  
-  int iter = 0;
-  while (read(fd, &cr, reclen) == reclen && iter < num_of_entries)
-  {
-      long c;
-      switch (cr.ut_type)
-      {
-      case 1:
-        // RUN_LVL, we need to skip this event
-        break;
-      case 8:
-        // DEAD_PROCESS, we need to dkip this event
-        break;
-      case 2:
-        // BOOT_TIME
-        printf("%s\t ", cr.ut_user); /* user */
-        printf("system boot \t ");
-        printf("%s\t", cr.ut_host); /* 5.11.0-38 generic (address) */
-        c = cr.ut_tv.tv_sec;
-        printf("%.20s\t", ctime(&c));
-        printf("\n");
-        break;               
-      default:
-        // all other events
-        printf("%s\t ", cr.ut_user); /* user */
-        printf("%s\t\t", cr.ut_line);
-        printf(" %s\t\t\t", cr.ut_host); /* 5.11.0-38 generic (address) */
-        c = cr.ut_tv.tv_sec;
-        printf("%.20s\t\n", ctime(&c));
-      }
-      ++iter;
-  }
-  
-
-  close (fd);
-  return 0;
-=======
-    int req_amount = atoi(argv[1]);
+    int req_amount = atoi(argv[FIRST_ARGV]);
     
     int fd;
     struct utmp cr;
@@ -115,7 +34,7 @@ int main(int argc, char *argv[])
     int total_amount = 0;
     while (read(fd, &cr, reclen) == reclen)
     {
-        if (cr.ut_type != 8 && cr.ut_type != 1){ total_amount++; }   
+        if (cr.ut_type != DEAD_PROCESS && cr.ut_type != RUN_LVL){ total_amount++; }   
     }
     close (fd);
 
@@ -130,7 +49,7 @@ int main(int argc, char *argv[])
     int i = total_amount - 1;
     while (read(fd, &cr, reclen) == reclen)
     {
-        if (cr.ut_type != 8 && cr.ut_type != 1){ records[i--] = cr; }
+        if (cr.ut_type != DEAD_PROCESS && cr.ut_type != RUN_LVL){ records[i--] = cr; }
     }
 
     for (size_t i = 0; (i < req_amount && i < total_amount); i++)
@@ -138,7 +57,7 @@ int main(int argc, char *argv[])
 
         records[i];
         long c;
-        if ((records[i].ut_type) == 2){
+        if ((records[i].ut_type) == BOOT_TIME){
 
             printf("%s\t ", records[i].ut_user); 
             printf("system boot \t ");
@@ -172,5 +91,4 @@ int main(int argc, char *argv[])
         
     close (fd);
     return 0;
->>>>>>> 60d54ebaf43d259386bc4c34169a95dd1ccb0239
 }
